40087f0f31724565ffb7b9ebbf56bc7d1551b181
Luiz Amaral <alaluizamaral@gmail.com>
2020-02-20 19:25:35 -0300

    Add todos os arquivos

---
+++ teste.py
@@ -0,0 +1,12 @@
+# import redis
+#
+# r = redis.StrictRedis(host = '172.17.0.2',
+# port=6379, charset="utf-8", decode_responses=True, db=0)
+#
+
+
+#quantidade_linhas = 9
+
+for x in range(0, 9)
+    valor[x]=+1
+
